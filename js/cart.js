2a7f6f5807a95f71b56962fb3e9b6d86c267bf0e
unknown <panchomachado00@gmail.com>
2023-10-07 01:41:35 -0300

    Add real time subtotal functionality

--- js/cart.js
+++ js/cart.js
@@ -1,24 +1,34 @@
-import getJSONData from './utils/getJSONData.js';
-import { CART_INFO_URL } from './constants/API.js';
+import getJSONData from "./utils/getJSONData.js";
+import { CART_INFO_URL } from "./constants/API.js";
 
-document.addEventListener('DOMContentLoaded', async () => {
-  const data = await getJSONData({ URL: CART_INFO_URL, options: '25801' });
+document.addEventListener("DOMContentLoaded", async () => {
+  const data = await getJSONData({ URL: CART_INFO_URL, options: "25801" });
   console.log(data);
   const articles = data.body.articles;
 
-  const tbody = document.querySelector('tbody');
+  const tbody = document.querySelector("tbody");
   articles.forEach((product) => {
-    const row = document.createElement('tr');
+    const row = document.createElement("tr");
+    row.className = "article";
 
     row.innerHTML = `
     <td>${product.name}</td>
-    <td>${product.unitCost}</td>
-    <td>${product.count}</td>
+    <td class="cost">${product.unitCost}</td>
+    <td><input type="number" value="${product.count}" style="width:5em"></td>
     <td>${product.currency}</td>
     <td><img src="${product.image}" alt="Imagen del product" width="50"></td>
-    <td>${product.unitCost * product.count}</td>
+    <td class="total">${product.unitCost * product.count}</td>
     `;
 
     tbody.appendChild(row);
+  });
+
+  document.querySelectorAll(".article").forEach((item) => {
+    const input = item.querySelector("input");
+    input.addEventListener("input", (e) => {
+      const quantity = e.target.value;
+      const cost = item.querySelector(".cost").innerText;
+      item.querySelector(".total").innerText = cost * quantity;
+    });
   });
 });
