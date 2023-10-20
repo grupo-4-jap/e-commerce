c6c8d4554f729d41acfb9f7df4475305b7f4f511
ghm7 <pozzisgnosis@outlook.com>
2023-10-20 08:21:03 -0300

    Refactor cart.js

--- js/cart.js
+++ js/cart.js
@@ -1,20 +1,41 @@
 import getJSONData from './utils/getJSONData.js';
 import { CART_INFO_URL } from './constants/API.js';
 
-function getProducts() {
-  const cart = JSON.parse(localStorage.getItem('cart'));
-  return cart != null ? cart : [];
+function isProductInCart(cartProducts, productID) {
+  if (cartProducts.length === 0) true;
+  for (let i = 0; i < cartProducts.length; i++) {
+    if (cartProducts[i].id !== productID) false;
+  }
+
+  return true;
+}
+
+async function getCartProducts() {
+  let localCart = JSON.parse(localStorage.getItem('cart'));
+  const defaultCartProduct = await getJSONData({
+    URL: CART_INFO_URL,
+    options: '25801',
+  }).then((data) => data.body.articles);
+
+  if (localCart !== null) {
+    console.log(isProductInCart(localCart, 50924));
+    return isProductInCart(localCart, 50924)
+      ? localCart
+      : localCart.concat(defaultCartProduct);
+  } else {
+    localCart = defaultCartProduct;
+  }
+
+  localStorage.setItem('cart', JSON.stringify(localCart));
+
+  return localCart;
 }
 
 document.addEventListener('DOMContentLoaded', async () => {
-  const data = await getJSONData({ URL: CART_INFO_URL, options: '25801' });
-  const articles = data.body.articles;
-
-  const cart = getProducts();
-  const shoppingCart = articles.concat(cart);
-
+  const cart = await getCartProducts();
   const tbody = document.querySelector('tbody');
-  shoppingCart.forEach((product) => {
+
+  cart.forEach((product) => {
     const { name, unitCost, count, currency, image } = product;
     const row = document.createElement('tr');
     row.className = 'article';
@@ -34,7 +55,6 @@ document.addEventListener('DOMContentLoaded', async () => {
 
   document.querySelectorAll('.article').forEach((item) => {
     const input = item.querySelector('input');
-    console.log(item);
     input.addEventListener('input', (e) => {
       const quantity = e.target.value;
       const productPrice = item.querySelector('.cost').innerText;
