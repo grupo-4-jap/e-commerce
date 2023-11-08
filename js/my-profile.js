365ae26a6c4d12c0f78641b485fa1be748642b89
ghm7 <pozzisgnosis@outlook.com>
2023-11-08 10:05:08 -0300

    Fix merge bug

--- js/my-profile.js
+++ js/my-profile.js
@@ -1,6 +1,8 @@
 import { getUserData } from './utils/loggingUser.js';
 
-const inputs = Array.from(document.querySelectorAll('input'));
+const inputs = Array.from(document.querySelectorAll('input')).filter(
+  (input) => input.name !== 'image-loader'
+);
 const form = document.querySelector('form');
 const firstName = document.getElementById('first-name');
 const lastName = document.getElementById('lastname');
@@ -8,6 +10,8 @@ const email = document.getElementById('my-profile-email');
 const nameFeedback = document.getElementById('name-feedback');
 const lastNameFeedback = document.getElementById('last-name-feedback');
 const emailFeedback = document.getElementById('email-feedback');
+const profilePictureInput = document.getElementById('profile-picture');
+const previewImage = document.getElementById('preview-image');
 
 function checkUserValidation() {
   if (firstName.value === '') {
@@ -60,39 +64,8 @@ document.addEventListener('DOMContentLoaded', (e) => {
     checkUserValidation();
 
     if (isValidated()) {
-      const updatedUserData = {};
+      const updatedUserData = new Object();
 
-
-  localStorage.setItem('userData', JSON.stringify(updatedUserData));
- }
-
-    const profilePictureInput = document.getElementById('profile-picture');
-    const previewImage = document.getElementById('preview-image');
-
-    profilePictureInput.addEventListener('change', function () {
-      const file = this.files[0];
-      if (file) {
-        const reader = new FileReader();
-
-        reader.onload = function (event) {
-          previewImage.src = event.target.result;
-          previewImage.style.display = 'block';
-        };
-
-        reader.addEventListener('load', () => {
-          localStorage.setItem('img', reader.result);
-        });
-
-        reader.readAsDataURL(file);
-      }
-    });
-      
-    const profileImage = localStorage.getItem('img') ?? false;
-
-    profileImage
-      ? (previewImage.src = profileImage)
-      : (previewImage.src = './icons/profile.svg');
-  
       inputs.forEach((input) => {
         const property = input.name;
         updatedUserData[`${property}`] = input.value;
@@ -100,4 +73,29 @@ document.addEventListener('DOMContentLoaded', (e) => {
 
       localStorage.setItem('userData', JSON.stringify(updatedUserData));
     }
+  });
+
+  profilePictureInput.addEventListener('change', function () {
+    const file = this.files[0];
+    if (file) {
+      const reader = new FileReader();
+
+      reader.onload = function (event) {
+        previewImage.src = event.target.result;
+        previewImage.style.display = 'block';
+      };
+
+      reader.addEventListener('load', () => {
+        localStorage.setItem('img', reader.result);
+      });
+
+      reader.readAsDataURL(file);
+    }
+  });
+
+  const profileImage = localStorage.getItem('img') ?? false;
+
+  profileImage
+    ? (previewImage.src = profileImage)
+    : (previewImage.src = './icons/profile.svg');
 });
