768a9f6891c3729890d874b9fca628d7a0d32208
ghm7 <pozzisgnosis@outlook.com>
2023-11-22 22:55:38 -0300

    Adjust some variables in my-profile.js

--- public/js/my-profile.js
+++ public/js/my-profile.js
@@ -6,10 +6,10 @@ const inputs = Array.from(document.querySelectorAll('input')).filter(
 const form = document.querySelector('form');
 const firstName = document.getElementById('first-name');
 const lastName = document.getElementById('lastname');
-const email = document.getElementById('my-profile-email');
+const email = document.getElementById('my-profile-username');
 const nameFeedback = document.getElementById('name-feedback');
 const lastNameFeedback = document.getElementById('last-name-feedback');
-const emailFeedback = document.getElementById('email-feedback');
+const emailFeedback = document.getElementById('username-feedback');
 const profilePictureInput = document.getElementById('profile-picture');
 const previewImage = document.getElementById('preview-image');
 
@@ -54,8 +54,8 @@ document.addEventListener('DOMContentLoaded', (e) => {
   const userData = getUserData();
 
   if (userData && userData.username) {
-    const emailInput = inputs.find((input) => input.name === 'email');
-    emailInput.value = userData.username;
+    const usernameInput = inputs.find((input) => input.name === 'username');
+    usernameInput.value = userData.username;
   }
 
   form.addEventListener('submit', (e) => {
