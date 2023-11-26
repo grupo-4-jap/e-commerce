b10917a2c17355ac5be29cddd7be388579395838
Gabriel Pozzi <pozzisgnosis@outlook.com>
2023-11-25 22:56:30 -0300

    Fix bug with the 3 main card in index, their request returns 404

--- public/js/index.js
+++ public/js/index.js
@@ -1,14 +1,6 @@
+import { CATEGORY } from './constants/CONSTANTS.js';
+import addEvents from './utils/addEvents.js';
+
 document.addEventListener('DOMContentLoaded', function () {
-  document.getElementById('autos').addEventListener('click', function () {
-    localStorage.setItem('catID', 101);
-    window.location = 'products.html';
-  });
-  document.getElementById('juguetes').addEventListener('click', function () {
-    localStorage.setItem('catID', 102);
-    window.location = 'products.html';
-  });
-  document.getElementById('muebles').addEventListener('click', function () {
-    localStorage.setItem('catID', 103);
-    window.location = 'products.html';
-  });
+  addEvents('card', CATEGORY);
 });
